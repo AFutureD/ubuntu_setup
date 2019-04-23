89c736c5e181437aef816c42b6e40e6f0721a77a
AFuture <afuture0@outlook.com>
2019-04-23 15:52:13 +0800

    update

--- setup.sh
+++ setup.sh
@@ -1,39 +1,44 @@
 #!/bin/bash
 
 install_tsinghua() {
-    sudo mv /etc/apt/sources.list /etc/apt/sources.list.backup
-    {
-        echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse"
-        echo "# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse"
-        echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse"
-        echo "# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse"
-        echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse"
-        echo "# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse"
-        echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted universe multiverse"
-        echo "# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted universe multiverse"
-    } >> /etc/apt/sources.list
+    if [[ -f /etc/apt/sources.list ]]; then
+        sudo mv /etc/apt/sources.list /etc/apt/sources.list.backup
+    else
+        sudo touch /etc/apt/sources.list
+    fi
+    sudo sh -c 'echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse" >> /etc/apt/sources.list'
+    sudo sh -c 'echo "# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse" >> /etc/apt/sources.list'
+    sudo sh -c 'echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse" >> /etc/apt/sources.list'
+    sudo sh -c 'echo "# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse" >> /etc/apt/sources.list'
+    sudo sh -c 'echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse" >> /etc/apt/sources.list'
+    sudo sh -c 'echo "# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse" >> /etc/apt/sources.list'
+    sudo sh -c 'echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted universe multiverse" >> /etc/apt/sources.list'
+    sudo sh -c 'echo "# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted universe multiverse" >> /etc/apt/sources.list'
+
 }
 
 install_basic() {
     sudo apt update
-    sudo apt install -y vim git openssh-server net-tools zsh ibus-pinyin ibus-sunpinyin
+    sudo apt-get remove -y --purge libreoffice\* thunderbird gnome-sudoku gnome-mahjongg gnome-mines aisleriot
+    sudo apt install -y vim git openssh-server net-tools zsh  curl ibus-pinyin ibus-sunpinyin
     ibus restart
     # oh my zsh
     # sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
     sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's:env zsh -l::g' | sed 's:chsh -s .*$::g')"
 
     # chrome 
-    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -o ./google-chrome-stable_current_amd64.deb
-    sudo dpkg -i ./google-chrome-stable_current_amd64.deb    
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O ./google-chrome-stable_current_amd64.deb
+    sudo dpkg -i ./google-chrome-stable_current_amd64.deb
+    sudo apt-get install -f
 }
 
 # shadowsocks in v2ray
 install_v2ray() {
-    bash <(curl -L -s https://install.direct/go.sh) 
+    curl -L -s https://install.direct/go.sh | bash 
     echo "export PATH=$PATH:/usr/bin/v2ray" >> ~/.bashrc
     sudo ln -s /usr/bin/v2ray/v2ray /usr/bin/v2ray
     sudo ln -s /usr/bin/v2ray/v2ctl /usr/bin/v2ctl
-    wget https://raw.githubusercontent.com/AFutureD/ubuntu_setup/master/v2ray_config.json -o ./v2ray_config.json
+    wget https://raw.githubusercontent.com/AFutureD/ubuntu_setup/master/v2ray_config.json -O ./v2ray_config.json
     sudo mv /etc/v2ray/config.json /etc/v2ray/config.json.backup
     sudo mv -f v2ray_config.json 
     sudo service v2ray start
@@ -44,9 +49,9 @@ install_anaconda() {
 
     continuum_website=https://repo.continuum.io/archive/
     latest_anaconda_steup=$(wget -q -O - $continuum_website index.html | grep "Anaconda3-" | grep "Linux" | grep "86_64" | head -n 1 | cut -d \" -f 2)
-    wget ${continuum_website}${latest_anaconda_steup} -o ./anacondaInstallScript.sh
+    wget ${continuum_website}${latest_anaconda_steup} -O ./anacondaInstallScript.sh
     sudo mkdir -p /opt/anaconda3 && sudo chmod ugo+w /opt/anaconda3
-    execute bash ./anacondaInstallScript.sh -f -b -p /opt/anaconda3
+    bash ./anacondaInstallScript.sh -f -b -p /opt/anaconda3
 
     # anaconda packages
     execute /opt/anaconda3/bin/conda update conda -y
@@ -56,8 +61,8 @@ install_anaconda() {
 }
 
 install_torch() {
-    wget https://download.pytorch.org/whl/cu100/torch-1.0.0-cp37-cp37m-linux_x86_64.whl -o ./torch-1.0.0-cp37-cp37m-linux_x86_64.whl
-    wget https://files.pythonhosted.org/packages/fb/01/03fd7e503c16b3dc262483e5555ad40974ab5da8b9879e164b56c1f4ef6f/torchvision-0.2.2.post3-py2.py3-none-any.whl -o ./torchvision-0.2.2.post3-py2.py3-none-any.whl
+    wget https://download.pytorch.org/whl/cu100/torch-1.0.0-cp37-cp37m-linux_x86_64.whl -O ./torch-1.0.0-cp37-cp37m-linux_x86_64.whl
+    wget https://files.pythonhosted.org/packages/fb/01/03fd7e503c16b3dc262483e5555ad40974ab5da8b9879e164b56c1f4ef6f/torchvision-0.2.2.post3-py2.py3-none-any.whl -O ./torchvision-0.2.2.post3-py2.py3-none-any.whl
 }
 
 setup_zshrc() {
@@ -84,5 +89,6 @@ main(){
 
     # effact zsh
     setup_zshrc
-    sudo chsh -s "which zsh"
+    sudo chsh -s $(which zsh)
 }
+main
