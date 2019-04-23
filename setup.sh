37879a180c67d0931d419cd00709dd5a496a4e99
AFutureD <afuture0@outlook.com>
2019-04-23 15:04:55 +0800

    restruct

--- setup.sh
+++ setup.sh
@@ -1,74 +1,88 @@
 #!/bin/bash
 
-sudo mv /etc/apt/sources.list /etc/apt/sources.list.backup
-{
-    echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse"
-    echo "# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse"
-    echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse"
-    echo "# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse"
-    echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse"
-    echo "# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse"
-    echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted universe multiverse"
-    echo "# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted universe multiverse"
-} >> /etc/apt/sources.list
+install_tsinghua() {
+    sudo mv /etc/apt/sources.list /etc/apt/sources.list.backup
+    {
+        echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse"
+        echo "# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse"
+        echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse"
+        echo "# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse"
+        echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse"
+        echo "# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse"
+        echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted universe multiverse"
+        echo "# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted universe multiverse"
+    } >> /etc/apt/sources.list
+}
 
-sudo apt update
-sudo apt install -y vim git openssh-server net-tools zsh ibus-pinyin ibus-sunpinyin
-ibus restart
-# oh my zsh
-# sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's:env zsh -l::g' | sed 's:chsh -s .*$::g')"
+install_basic() {
+    sudo apt update
+    sudo apt install -y vim git openssh-server net-tools zsh ibus-pinyin ibus-sunpinyin
+    ibus restart
+    # oh my zsh
+    # sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's:env zsh -l::g' | sed 's:chsh -s .*$::g')"
 
-# chrome 
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -o ./google-chrome-stable_current_amd64.deb
-sudo dpkg -i ./google-chrome-stable_current_amd64.deb
+    # chrome 
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -o ./google-chrome-stable_current_amd64.deb
+    sudo dpkg -i ./google-chrome-stable_current_amd64.deb    
+}
 
-# TODO VSCODE、CHROME、 torch 
-
-execute bash ./beautify.sh
-
-## shadowsocks in v2ray
-# bash <(curl -L -s https://install.direct/go.sh) 
-# echo "export PATH=$PATH:/usr/bin/v2ray" >> ~/.bashrc
-# sudo ln -s /usr/bin/v2ray/v2ray /usr/bin/v2ray
-# sudo ln -s /usr/bin/v2ray/v2ctl /usr/bin/v2ctl
-# sudo mv /etc/v2ray/config.json /etc/v2ray/config.json.backup
-# sudo mv -f v2ray_config.json 
-# sudo service v2ray start
-
-
-# wget https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh -O ./anaconda.sh
-# yes | bash ./anaconda.sh
+# shadowsocks in v2ray
+install_v2ray() {
+    bash <(curl -L -s https://install.direct/go.sh) 
+    echo "export PATH=$PATH:/usr/bin/v2ray" >> ~/.bashrc
+    sudo ln -s /usr/bin/v2ray/v2ray /usr/bin/v2ray
+    sudo ln -s /usr/bin/v2ray/v2ctl /usr/bin/v2ctl
+    wget https://raw.githubusercontent.com/AFutureD/ubuntu_setup/master/v2ray_config.json -o ./v2ray_config.json
+    sudo mv /etc/v2ray/config.json /etc/v2ray/config.json.backup
+    sudo mv -f v2ray_config.json 
+    sudo service v2ray start
+}
 
 # install anaconda
-continuum_website=https://repo.continuum.io/archive/
-latest_anaconda_steup=$(wget -q -O - $continuum_website index.html | grep "Anaconda3-" | grep "Linux" | grep "86_64" | head -n 1 | cut -d \" -f 2)
-wget ${continuum_website}${latest_anaconda_steup} -o ./anacondaInstallScript.sh
-sudo mkdir -p /opt/anaconda3 && sudo chmod ugo+w /opt/anaconda3
-execute bash ./anacondaInstallScript.sh -f -b -p /opt/anaconda3
+install_anaconda() {
 
-# anaconda packages
-execute /opt/anaconda3/bin/conda update conda -y
-execute /opt/anaconda3/bin/conda clean --all -y
-execute /opt/anaconda3/bin/conda install ipython -y
-execute /opt/anaconda3/bin/pip install numpy scipy matplotlib scikit-learn scikit-image jupyter notebook pandas
+    continuum_website=https://repo.continuum.io/archive/
+    latest_anaconda_steup=$(wget -q -O - $continuum_website index.html | grep "Anaconda3-" | grep "Linux" | grep "86_64" | head -n 1 | cut -d \" -f 2)
+    wget ${continuum_website}${latest_anaconda_steup} -o ./anacondaInstallScript.sh
+    sudo mkdir -p /opt/anaconda3 && sudo chmod ugo+w /opt/anaconda3
+    execute bash ./anacondaInstallScript.sh -f -b -p /opt/anaconda3
 
-# torch 
-# wget https://download.pytorch.org/whl/cu100/torch-1.0.0-cp37-cp37m-linux_x86_64.whl -o ./torch-1.0.0-cp37-cp37m-linux_x86_64.whl
-# wget https://files.pythonhosted.org/packages/fb/01/03fd7e503c16b3dc262483e5555ad40974ab5da8b9879e164b56c1f4ef6f/torchvision-0.2.2.post3-py2.py3-none-any.whl -o ./torchvision-0.2.2.post3-py2.py3-none-any.whl
+    # anaconda packages
+    execute /opt/anaconda3/bin/conda update conda -y
+    execute /opt/anaconda3/bin/conda clean --all -y
+    execute /opt/anaconda3/bin/conda install ipython -y
+    execute /opt/anaconda3/bin/pip install numpy scipy matplotlib scikit-learn scikit-image jupyter notebook pandas
+}
 
+install_torch() {
+    wget https://download.pytorch.org/whl/cu100/torch-1.0.0-cp37-cp37m-linux_x86_64.whl -o ./torch-1.0.0-cp37-cp37m-linux_x86_64.whl
+    wget https://files.pythonhosted.org/packages/fb/01/03fd7e503c16b3dc262483e5555ad40974ab5da8b9879e164b56c1f4ef6f/torchvision-0.2.2.post3-py2.py3-none-any.whl -o ./torchvision-0.2.2.post3-py2.py3-none-any.whl
+}
 
-# PATH
-# {
-#     # echo "export PATH=$PATH:/usr/bin/v2ray"
-#     echo "export PATH=$PATH:/opt/anaconda3/bin"
-# } >> ~/.zshrc
+setup_zshrc() {
+    # PATH
+    {
+        # echo "export PATH=$PATH:/usr/bin/v2ray"
+        echo "export PATH=$PATH:/opt/anaconda3/bin"
+    } >> ~/.zshrc
 
-# alias
-# {
-#     echo "alias jn=\"jupyter notebook\""
-#     echo "alias jl=\"jupyter lab\""
-# } >> ~/.zshrc
+    # alias
+    # {
+    #     echo "alias jn=\"jupyter notebook\""
+    #     echo "alias jl=\"jupyter lab\""
+    # } >> ~/.zshrc
+}
 
-# effact zsh
-sudo chsh -s "which zsh"
+main(){
+    install_tsinghua
+    install_basic
+    # beautify
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/AFutureD/ubuntu_setup/master/beautify.sh)"
+    # install_anaconda
+    # install_v2ray
+
+    # effact zsh
+    setup_zshrc
+    sudo chsh -s "which zsh"
+}
